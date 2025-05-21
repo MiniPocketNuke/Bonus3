204f827681784ef6ad4236840204411bcbf16f4e
Mah <timohaarsentiev1@gmail.com>
2025-05-21 11:12:01 +0300

    Initial Commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int a) {
+        int bonusRequirement = 20;
+        int result = a / bonusRequirement;
+        return result;
+    }
+
+}
